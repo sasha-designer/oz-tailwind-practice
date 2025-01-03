edfadf518e7780c02bae178e345c8e8fecbe4d35
sasha <sasha.park.designer@gmail.com>
2025-01-03 13:49:16 +0900

    content 적용

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <main>
       <Header />
-      <div>a</div>
       {/* <section>  */}
       <section className="flex flex-wrap justify-center gap-5 px-5 py-10"> 
         {contents.map((el) => (
